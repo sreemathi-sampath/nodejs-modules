279deae8af01213d9b1bdcd93aa0d6d275a2a0d9
Sreemathi Sampath <sreemathisampath55@gmail.com>
2024-05-29 23:24:13 +0530

    handled uncaught exception

--- app/start.js
+++ app/start.js
@@ -1,7 +1,12 @@
-const http = require("http");
-
+import http from 'http';
+import process from 'process';
 const server = http.createServer(function (_req, res) {
   res.writeHead(200);
   res.end("Hello, World!");
 });
 server.listen(8080);
+
+process.on('uncaughtException', err => {
+  console.log(`Uncaught Exception: ${err.message}`)
+  process.exit(1)
+})
